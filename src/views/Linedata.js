9e500be88fc7f0f6e61df067c84a33fdfa5d14e5
Gunasekaran92 <35014357+Gunasekaran92@users.noreply.github.com>
2021-04-26 19:40:27 -0400

    Update Linedata.js

--- src/views/Linedata.js
+++ src/views/Linedata.js
@@ -1,6 +1,6 @@
 
 async function getLine() {
-    return await fetch('http://test-app.canadacentral.azurecontainer.io/line')
+    return await fetch('https://exp-app.azurewebsites.net/line')
         .then(data2 => data2.json())
         .then(data => data)
 }

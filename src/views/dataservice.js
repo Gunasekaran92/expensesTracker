6e6fc459929348cdb69319256f3ab1d8a17ff03f
Gunasekaran92 <35014357+Gunasekaran92@users.noreply.github.com>
2021-04-26 20:04:14 -0400

    Update dataservice.js

--- src/views/dataservice.js
+++ src/views/dataservice.js
@@ -2,9 +2,6 @@
 import getDonut from './Donutdata';
 import getLine from './Linedata';
 import getSummary from './Summarydata';
-import getMetricL from './metricL';
-
-
 
 function getRandomDelay() {
     return 500 + (Math.random() * 2000);
@@ -33,25 +30,6 @@ export function getSummaryData() {
     return new Promise((resolve) => {
         setTimeout(() => {
             resolve(getSummary());
-        }, getRandomDelay());
-    })
-}
-
-
-
-/*
-export function getMetricData() {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve(getMetric());
-        }, getRandomDelay());
-    })
-}*/
-
-export function getMetricDataL() {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve(getMetricL());
         }, getRandomDelay());
     })
 }
